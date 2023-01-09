d6a56685a1610107ffd3a76ba340c143ff06fda2
Marinidis Antonios (IFKL CSC FI ADI DES) <antonis.marinidis7@gmail.com>
2023-01-09 19:30:09 +0100

    done with the picking both ways and display them

---
+++ src/PickedDataProvider.ts
@@ -0,0 +1,58 @@
+import * as vscode from 'vscode';
+import { Items } from './Items';
+import { SettingsAccess } from './SettingsAccess';
+
+export class PickedDataProvider implements vscode.TreeDataProvider<any> {
+
+    items: Items[] = [];
+
+    private _onDidChangeTreeData: vscode.EventEmitter<Items | undefined | null | void> = new vscode.EventEmitter<Items | undefined | null | void>();
+    readonly onDidChangeTreeData: vscode.Event<Items | undefined | null | void> = this._onDidChangeTreeData.event;
+
+    refresh(): void {
+        this._onDidChangeTreeData.fire();
+    }
+
+    getTreeItem(element: Items): vscode.TreeItem | Thenable<vscode.TreeItem> {
+        return element;
+    }
+
+    getChildren(element?: any): Items[] {
+
+        if (element) {
+            return element.children;
+        } else {
+            if (this.items.length === 0) {
+                return [];
+            } else {
+                return this.items;
+            }
+        }
+
+    }
+
+    insert(itemName: string, contextValue: string) {
+
+        const item = new Items(itemName, vscode.TreeItemCollapsibleState.None, contextValue);
+        let flag: boolean = true;
+
+        if (this.items.length === 0) {
+            this.items?.push(item);
+        } else {
+            for (let i = 0; i < this.items.length; i++) {
+                if (this.items[i].label === itemName) {
+                    flag = false;
+                    return flag;
+                }
+            }
+            this.items?.push(item);
+        }
+        return flag;
+    }
+
+    delete(item: string): void {
+
+        this.items = this.items.filter((target: any) => target.label !== item);
+
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
+import { Items } from './Items';
 import { TreeDataProvider } from "./TreeDataProvider";
+import { PickedDataProvider } from "./PickedDataProvider";
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -20,6 +22,40 @@ export function activate(context: vscode.ExtensionContext) {
 		canSelectMany: true
 
 	});
+
+	const pickedDataProvider = new PickedDataProvider();
+
+	vscode.window.createTreeView('todaysTodoList', {
+
+		treeDataProvider: pickedDataProvider,
+		canSelectMany: true
+
+	});
+
+	vscode.commands.registerCommand('pickPrimaryItem', (item: Items) => {
+
+		pickAndDisplay(item);
+		
+	});
+
+	vscode.commands.registerCommand('pickPrimaryItem.inline', (item: Items) => {
+
+		pickAndDisplay(item);
+		
+	});
+
+	function pickAndDisplay(item: Items) {
+
+		const insert: boolean | undefined = pickedDataProvider.insert(item.label, item.contextValue);
+
+		if (insert) {
+			pickedDataProvider.refresh();
+			vscode.window.showInformationMessage(item.label + " : has picked from " + item.contextValue + " to Rollout");
+		} else {
+			vscode.window.showErrorMessage(item.label + " : already exists from " + item.contextValue + " to Rollout");
+		}
+
+	}
 
 }
 
